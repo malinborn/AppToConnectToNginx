0ec2068334264cdfb3750cf8441bc0c3e3a2ac9b
malinborn <rikkeisy@gmail.com>
2023-11-05 22:52:58 +0300

    old docker file

---
+++ AppToConnectToNginx/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder();
+
+var app =  builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.MapGet("/", () => "Hello, do you remember me?");
+
+app.Run();
